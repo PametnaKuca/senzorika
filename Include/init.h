4d7fce80f1a381a9320b0fd8caecc362545fd2fe
PametnaKuca <pametna.kucagl@gmail.com>
2017-09-08 08:02:48 +0200

    Pinnspack instructions added

--- Include/init.h
+++ Include/init.h
@@ -69,7 +69,7 @@
 #define MFRC522_CS_RCC					RCC_AHB1Periph_GPIOA
 #define MFRC522_CS_PORT					GPIOA
 #define MFRC522_CS_PIN					GPIO_Pin_8
-
+/* For SPIx and TM_SPI_PinsPack_x definitions look at tm_stm32f4_spi.h */
 #define MFRC522_SPI						SPI1
 #define MFRC522_SPI_PINSPACK			TM_SPI_PinsPack_2
 /* ------------------------------------------------------*/
